17ff2b9c2e63fc5c3be05cbcc780356f33ab8fda
s26126 <s26126@pjwstk.edu.pl>
2026-05-08 23:17:12 +0200

    RoomsController - endpointy GET (lista, filtrowanie, po id, po budynku)

--- APBD-s26126-TrainingCenter/Controllers/RoomsController.cs
+++ APBD-s26126-TrainingCenter/Controllers/RoomsController.cs
@@ -1,6 +1,53 @@
-namespace APBD_s26126_TrainingCenter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using APBD_s26126_TrainingCenter.Data;
 
-public class RoomsController
+namespace APBD_s26126_TrainingCenter.Controllers
 {
-    
+    // api/rooms
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+
+        // GET api/rooms
+        // GET api/rooms?minCapacity=20&hasProjector=true&activeOnly=true
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] int? minCapacity, [FromQuery] bool? hasProjector, [FromQuery] bool? activeOnly)
+        {
+            var rooms = Data.Data.Rooms.AsEnumerable();
+
+            if (minCapacity.HasValue)
+                rooms = rooms.Where(r => r.Capacity >= minCapacity.Value);
+
+            if (hasProjector.HasValue)
+                rooms = rooms.Where(r => r.HasProjector == hasProjector.Value);
+
+            if (activeOnly == true)
+                rooms = rooms.Where(r => r.IsActive);
+
+            return Ok(rooms.ToList());
+        }
+
+        // GET api/rooms/building/{buildingCode}
+        [HttpGet("building/{buildingCode}")]
+        public IActionResult GetByBuilding([FromRoute] string buildingCode)
+        {
+            var rooms = Data.Data.Rooms.Where(r => r.BuildingCode == buildingCode).ToList();
+
+            return Ok(rooms);
+        }
+
+        // GET api/rooms/{id}
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var room = Data.Data.Rooms.FirstOrDefault(r => r.Id == id);
+
+            if (room == null)
+                return NotFound();
+
+            return Ok(room);
+        }
+
+    }
 }

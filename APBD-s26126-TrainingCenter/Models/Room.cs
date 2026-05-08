f9cb636e951c2c989d8d9227064be459b32dbd07
s26126 <s26126@pjwstk.edu.pl>
2026-05-08 22:23:48 +0200

    Dodanie walidacji do modelu Room (Required,   Range)

--- APBD-s26126-TrainingCenter/Models/Room.cs
+++ APBD-s26126-TrainingCenter/Models/Room.cs
@@ -7,8 +7,10 @@ public class Room
     public int Id { get; set; }
     [Required]
     public string Name { get; set; } = null!;
+    [Required]
     public string BuildingCode { get; set; } = null!;
     public int Floor { get; set; }
+    [Range(1, int.MaxValue)]
     public int Capacity { get; set; }
     public bool HasProjector { get; set; }
     public bool IsActive { get; set; }

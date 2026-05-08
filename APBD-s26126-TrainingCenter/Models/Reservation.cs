f0ac2c2620f77eb62e8cb325b26b89e26b72ce7c
s26126 <s26126@pjwstk.edu.pl>
2026-05-08 22:26:03 +0200

    Dodanie walidacji do modelu Reservation (Required)

--- APBD-s26126-TrainingCenter/Models/Reservation.cs
+++ APBD-s26126-TrainingCenter/Models/Reservation.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace APBD_s26126_TrainingCenter.Models;
 
 public class Reservation
 {
     public int Id { get; set; }
     public int RoomId { get; set; }
+    [Required]
     public string OrganizerName { get; set; } = null!;
+    [Required]
     public string Topic { get; set; } = null!;
     public DateOnly Date { get; set; }
     public TimeOnly StartTime { get; set; }

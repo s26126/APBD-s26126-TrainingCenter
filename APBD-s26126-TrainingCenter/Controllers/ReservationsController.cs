a6b53623a5d29a320d53583beebbfb06d5dceed0
s26126 <s26126@pjwstk.edu.pl>
2026-05-09 20:13:43 +0200

    ReservationsController - endpointy GET (lista, filtrowanie, po id)

--- APBD-s26126-TrainingCenter/Controllers/ReservationsController.cs
+++ APBD-s26126-TrainingCenter/Controllers/ReservationsController.cs
@@ -1,6 +1,46 @@
-namespace APBD_s26126_TrainingCenter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using APBD_s26126_TrainingCenter.Data;
+using APBD_s26126_TrainingCenter.Models;
 
-public class ReservationsController
+namespace APBD_s26126_TrainingCenter.Controllers
 {
-    
+    // api/reservations
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+
+        // GET api/reservations
+        // GET api/reservations?date=2026-05-10&status=confirmed&roomId=2
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] DateOnly? date,[FromQuery] string? status, [FromQuery] int? roomId)
+        {
+            var reservations = Data.Data.Reservations.AsEnumerable();
+
+            if (date.HasValue)
+                reservations = reservations.Where(r => r.Date == date.Value);
+
+            if (!string.IsNullOrEmpty(status) && Enum.TryParse<ReservationStatus>(status, true, out var parsedStatus))
+                    reservations = reservations.Where(r => r.Status == parsedStatus);
+
+            if (roomId.HasValue)
+              reservations = reservations.Where(r => r.RoomId == roomId.Value);
+
+            return Ok(reservations.ToList());
+        }
+
+
+        // GET api/reservations/{id}
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+          var reservation = Data.Data.Reservations.FirstOrDefault(r => r.Id == id);
+
+            if (reservation == null)
+                return NotFound();
+
+            return Ok(reservation);
+        }
+
+    }
 }
